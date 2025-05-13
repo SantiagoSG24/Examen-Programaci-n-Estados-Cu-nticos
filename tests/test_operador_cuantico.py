c458b7d01b88aa969511a9288a0ffb4001163607
Santiago Sosa <ssosag@student.universidadunie.com>
2025-05-13 10:19:11 +0200

    cambios test operador cuantico

--- tests/test_operador_cuantico.py
+++ tests/test_operador_cuantico.py
@@ -1,21 +1,45 @@
-from test_estado_cuantico import EstadoCuantico
-from test_operador_cuantico import OperadorCuantico
 import unittest
-from typing import List, Dict, Union
-import math
-import numpy as np
-import json
+from src.operador_cuantico import OperadorCuantico, crear_operador_x, crear_operador_h
+from src.estado_cuantico import EstadoCuantico
 
 class TestOperadorCuantico(unittest.TestCase):
-    def test_aplicar_operador_X(self):
-        estado = EstadoCuantico("q0", [1, 0], "computacional")
-        opX = OperadorCuantico("X", [[0, 1], [1, 0]])
-        nuevo_estado = opX.aplicar(estado)
-        self.assertEqual(nuevo_estado.vector.tolist(), [0, 1])
+    def test_operador_x(self):
+        op_x = crear_operador_x()
+        self.assertEqual(op_x.nombre, "X")
+        
+        # Aplicar X a |0> debe dar |1>
+        estado0 = EstadoCuantico("q0", [1, 0])
+        estado1 = op_x.aplicar(estado0)
+        self.assertAlmostEqual(estado1.vector[0], 0)
+        self.assertAlmostEqual(estado1.vector[1], 1)
+        
+        # Aplicar X a |1> debe dar |0>
+        estado1_orig = EstadoCuantico("q1", [0, 1])
+        estado0_result = op_x.aplicar(estado1_orig)
+        self.assertAlmostEqual(estado0_result.vector[0], 1)
+        self.assertAlmostEqual(estado0_result.vector[1], 0)
+    
+    def test_operador_h(self):
+        op_h = crear_operador_h()
+        self.assertEqual(op_h.nombre, "H")
+        
+        # Aplicar H a |0> debe dar |+>
+        estado0 = EstadoCuantico("q0", [1, 0])
+        estado_plus = op_h.aplicar(estado0)
+        h = 1/2**0.5  # 1/sqrt(2)
+        self.assertAlmostEqual(estado_plus.vector[0], h)
+        self.assertAlmostEqual(estado_plus.vector[1], h)
+        
+        # Aplicar H dos veces debe devolver al estado original
+        estado_original = op_h.aplicar(estado_plus)
+        self.assertAlmostEqual(estado_original.vector[0], 1, places=5)
+        self.assertAlmostEqual(estado_original.vector[1], 0, places=5)
+    
+    def test_dimension_incompatible(self):
+        op = OperadorCuantico("test", [[1, 0], [0, 1]])
+        estado = EstadoCuantico("q_err", [1, 0, 0])  # 3 componentes
+        with self.assertRaises(ValueError):
+            op.aplicar(estado)
 
-    def test_aplicar_operador_H(self):
-        estado = EstadoCuantico("q0", [1, 0], "computacional")
-        opH = OperadorCuantico("H", [[1/np.sqrt(2), 1/np.sqrt(2)], [1/np.sqrt(2), -1/np.sqrt(2)]])
-        nuevo_estado = opH.aplicar(estado)
-        self.assertAlmostEqual(nuevo_estado.vector[0], 1/np.sqrt(2))
-        self.assertAlmostEqual(nuevo_estado.vector[1], 1/np.sqrt(2))
+if __name__ == "__main__":
+    unittest.main()
